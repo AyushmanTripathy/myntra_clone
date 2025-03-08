6870a3493aeec2ed4ffa3266dc879717b25d1590
AyushmanTripathy <ayushmantripathy2004@gmail.com>
2025-03-08 15:05:05 +0530

    Added responsive grid

--- src/lib/components/CardsContainer.svelte
+++ src/lib/components/CardsContainer.svelte
@@ -3,8 +3,10 @@
   const { products } = $props();
 </script>
 
-<div class="grow grid grid-cols-5 gap-3">
-  {#each products as product }
-    <Card product={product} />
+<div
+  class="grow grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-3"
+>
+  {#each products as product}
+    <Card {product} />
   {/each}
 </div>
