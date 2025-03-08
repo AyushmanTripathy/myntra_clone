5f7dc4d516f3306f7e20111d66c3e3d8d56aa7b2
AyushmanTripathy <ayushmantripathy2004@gmail.com>
2025-03-08 13:17:15 +0530

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+</script>
+
+<main class="flex h-screen w-screen justify-center items-center">
+  Hello
+</main>
+
+<style lang="postcss">
+  @reference "tailwindcss";
+  :global(html) {
+    background-color: theme(--color-gray-100);
+  }
+</style>
